ecc7fe3551747b68c86d7dca615d465f29a7cf46
tamaina <tamaina@hotmail.co.jp>
2018-08-18 23:02:25 +0900

    だめですね…

--- scripts/highl.js
+++ scripts/highl.js
@@ -5,13 +5,13 @@ function fuckescape(val){
   return val
 }
 module.exports = (htm) => {
-  htm = htm.replace(/<code class="(.*?)">([\s\S]*?)<\/code>/gi, function(a, l, b){
+  htm = htm.replace(/<pre class="(.*?)">\s*?<code>([\s\S]*?)<\/code>\s*?<\/pre>/gi, function(a, l, b){
     b = fuckescape(b)
     if(l.indexOf("lang-") == 0) l = l.slice(5)
     const hled = highlighter(b, l)
-    return `<code class="highlighted${l ? ` ${l}` : ''}">${hled}</code>`
+    return `<pre class="highlighted${l ? ` ${l}` : ''}"><code>${hled}</code></code>`
   } )
-  htm = htm.replace(/<pre>\s*?<code>([\s\S]*?)<\/code>\s*?<\/pre>/gi, function(a, b){
+  htm = htm.replace(/<code>([\s\S]*?)<\/code>/gi, function(a, b){
     b = fuckescape(b)
     const hled = highlighter(b)
     return `<code class="highlighted">${hled}</code>`
