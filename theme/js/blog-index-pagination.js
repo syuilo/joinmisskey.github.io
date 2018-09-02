4ceabdc842acc6cf4d2216259ea75969b29675e3
tamaina <tamaina@hotmail.co.jp>
2018-09-02 11:35:19 +0900

    wip

--- theme/js/blog-index-pagination.js
+++ theme/js/blog-index-pagination.js
@@ -8,33 +8,35 @@ function addListenerBlogIndexpagination(){
     for (let pagination of paginations) {
         pagination.onclick = togglePage
         function togglePage(){
-            console.log('はっか')
-            for (let pa of paginations) {
-                if (pa.classList.contains('active')) pa.classList.remove('active')
-            }
-            pagination.classList.add('active')
             const num = Number(pagination.textContent) - 1
-            for (i = 0; i < articles.length; i++){
-                if(!b(i, num)) {
-                    articles[i].classList.remove('d-block')
-                    articles[i].classList.add('d-none')
-                }
-            }
-            for (i = 0; i < articles.length; i++){
-                if(b(i, num)) {
-                    articles[i].classList.remove('d-none')
-                    articles[i].classList.add('d-block')
-                }
-            }
-            window.scroll({
-                top: 0,
-                behavior: "smooth"
-            })
+            toggleArticles(num, articles, paginations)
         }
     }
+    const search = RegExp(/pages=([0-9]+)/i).exec(window.location.search)
+    if (search) toggleArticles(search[1] - 1, articles, paginations)
 }
 function b(i, num){ return num * 15 <= i && i < (num + 1) * 15 }
-
+function toggleArticles(num, articles, paginations) {
+    for (i = 0; i < articles.length; i++){
+        if(!b(i, num)) {
+            articles[i].classList.remove('d-block')
+            articles[i].classList.add('d-none')
+        }
+        if(b(i, num)) {
+            articles[i].classList.remove('d-none')
+            articles[i].classList.add('d-block')
+        }
+    }
+    window.scroll({
+        top: 0,
+        behavior: "smooth"
+    })
+    for (let pa of paginations) {
+        if (pa.classList.contains('active')) pa.classList.remove('active')
+    }
+    pagination.classList.add('active')
+    history.pushState()
+}
 
 window.addEventListener('DOMContentLoaded', addListenerBlogIndexpagination)
 window.addEventListener('pjax:load', addListenerBlogIndexpagination)
