fd3d049ecedbf148c7e677d95a8d1e9798a56213
tamaina <tamaina@hotmail.co.jp>
2018-09-11 00:40:13 +0900

    oops

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,60 @@
 importScripts('/workbox-sw.js');
 
-workbox.precaching.precacheAndRoute([]);
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "assets/animate.css",
+    "revision": "d96b2083b0acbb11911bb4f068158299"
+  },
+  {
+    "url": "assets/animate.min.css",
+    "revision": "c78e4003414fbf2814dc097a5e1c784a"
+  },
+  {
+    "url": "assets/bootstrap.bundle.js",
+    "revision": "50a98c751c19ae5ea4fc42b2ba2da89b"
+  },
+  {
+    "url": "assets/bootstrap.bundle.min.js",
+    "revision": "ef58fee438cd2da2c3b33ff6f1cfeebf"
+  },
+  {
+    "url": "assets/bootstrap.js",
+    "revision": "4bc939cd6b79a562e8d14bc7a4674520"
+  },
+  {
+    "url": "assets/bootstrap.min.js",
+    "revision": "67176c242e1bdc20603c878dee836df3"
+  },
+  {
+    "url": "assets/fontawesome.min.css",
+    "revision": "66792e9db1ace7288d95debe0a9b2c7f"
+  },
+  {
+    "url": "assets/jquery.slim.min.js",
+    "revision": "d0c25b95f8c4fc5bfdbdb358996ea3f4"
+  },
+  {
+    "url": "assets/main.js",
+    "revision": "f0b1d3812219ba82fd7c8fabc622ea3d"
+  },
+  {
+    "url": "assets/main.min.js",
+    "revision": "e11d9c12f7cf0835f1b26a69a9439d08"
+  },
+  {
+    "url": "assets/pjax-api.js",
+    "revision": "b1cf3b41d7e10753b12b268df38ee938"
+  },
+  {
+    "url": "assets/pjax-api.min.js",
+    "revision": "73441b2e299762517990d794c5444153"
+  },
+  {
+    "url": "assets/style.min.css",
+    "revision": "f33af031c903be5762d78de16b7d78b1"
+  },
+  {
+    "url": "workbox-sw.js",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  }
+]);
