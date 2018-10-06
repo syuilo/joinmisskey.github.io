2c5553569eef7aefbf9f9897943f213f1c6f6db2
tamaina <tamaina@hotmail.co.jp>
2018-10-07 01:03:02 +0900

    update

--- service_worker.js
+++ service_worker.js
@@ -2,30 +2,6 @@ importScripts('/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "assets/animate.css",
-    "revision": "d96b2083b0acbb11911bb4f068158299"
-  },
-  {
-    "url": "assets/animate.min.css",
-    "revision": "c78e4003414fbf2814dc097a5e1c784a"
-  },
-  {
-    "url": "assets/bootstrap.bundle.js",
-    "revision": "50a98c751c19ae5ea4fc42b2ba2da89b"
-  },
-  {
-    "url": "assets/bootstrap.bundle.min.js",
-    "revision": "ef58fee438cd2da2c3b33ff6f1cfeebf"
-  },
-  {
-    "url": "assets/bootstrap.js",
-    "revision": "4bc939cd6b79a562e8d14bc7a4674520"
-  },
-  {
-    "url": "assets/bootstrap.min.js",
-    "revision": "67176c242e1bdc20603c878dee836df3"
-  },
-  {
     "url": "assets/fontawesome.min.css",
     "revision": "68ee62db13e0ab881b065e170e1ebbe1"
   },
@@ -40,14 +16,6 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "assets/main.min.js",
     "revision": "e11d9c12f7cf0835f1b26a69a9439d08"
-  },
-  {
-    "url": "assets/pjax-api.js",
-    "revision": "b1cf3b41d7e10753b12b268df38ee938"
-  },
-  {
-    "url": "assets/pjax-api.min.js",
-    "revision": "73441b2e299762517990d794c5444153"
   },
   {
     "url": "assets/style.min.css",
