de78c2ef0411bff9f39342a4be9000c3084e94f1
tamaina <tamaina@hotmail.co.jp>
2018-10-30 18:26:19 +0900

    :v:

--- push7-worker.js
+++ push7-worker.js
@@ -1,5 +1,5 @@
 /* workbox */
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 workbox.routing.registerRoute(
     /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
     workbox.strategies.staleWhileRevalidate({
