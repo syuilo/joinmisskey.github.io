95b32e4e262eba78b3ac839f05c13d4e8937ef54
tamaina <tamaina@hotmail.co.jp>
2018-11-10 03:08:02 +0900

    update

--- push7-worker.js
+++ push7-worker.js
@@ -1,4 +1,4 @@
-/* workbox 2018-11-05T13:54:29.114Z */
+/* workbox 2018-11-09T18:05:05.678Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 workbox.routing.registerRoute(
     /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
@@ -9,7 +9,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: '/offline/',
-        revision: '1541426069114',
+        revision: '1541786705678',
     }
 ]);
 self.addEventListener('fetch', function(event) {
