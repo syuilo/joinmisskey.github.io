8e399f8fa8e15ca2b5360f5bded94061baa33eb5
tamaina <tamaina@hotmail.co.jp>
2018-11-11 04:53:30 +0900

    add French

--- push7-worker.js
+++ push7-worker.js
@@ -1,4 +1,4 @@
-/* workbox 2018-11-10T11:53:08.814Z */
+/* workbox 2018-11-10T19:46:42.483Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 workbox.routing.registerRoute(
     /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
@@ -9,7 +9,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: '/offline/',
-        revision: '1541850788814',
+        revision: '1541879202483',
     }
 ]);
 self.addEventListener('fetch', function(event) {
