5d3be088b915e0f33e0cb211ecac2558dc4aa733
tamaina <tamaina@hotmail.co.jp>
2018-11-12 05:24:20 +0900

    build

--- copy-ja.bat
+++ copy-ja.bat
@@ -4,9 +4,11 @@ git add --all
 git commit -a -m %1
 git push
 cd ../en
+git pull origin pages/en
 git pull origin pages/ja
 git push
 cd ../fr
+git pull origin pages/fr
 git pull origin pages/ja
 git push
 cd ../..
--- copy-ja.sh
+++ copy-ja.sh
@@ -5,8 +5,11 @@ git add --all
 git commit -a -m %1
 git push
 cd ../en
+git pull origin pages/en
 git pull origin pages/ja
 git push
 cd ../fr
+git pull origin pages/en
 git pull origin pages/ja
 git push
+cd ../..
