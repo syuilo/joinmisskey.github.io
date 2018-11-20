aac7f0177d42e13911db2ea0849bc80472cda7d7
tamaina <tamaina@hotmail.co.jp>
2018-11-20 20:13:33 +0900

    discord

--- push7-worker.js
+++ push7-worker.js
@@ -1,4 +1,4 @@
-/* workbox 2018-11-19T16:29:40.341Z */
+/* workbox 2018-11-20T11:10:51.784Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 workbox.routing.registerRoute(
     /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
@@ -9,7 +9,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: '/offline/',
-        revision: '1542644980341',
+        revision: '1542712251784',
     }
 ]);
 self.addEventListener('fetch', function(event) {
