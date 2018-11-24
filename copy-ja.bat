b5eaa4f82ad8c524e2ce62e5b70a931203bba715
tamaina <tamaina@hotmail.co.jp>
2018-11-24 21:06:23 +0900

    :v:

--- copy-ja.bat
+++ copy-ja.bat
@@ -4,10 +4,12 @@ git add --all
 git commit -a -m %1
 git push
 cd ../en
+git commit -a -m %1
 git pull origin pages/en
 git pull origin pages/ja
 git push
 cd ../fr
+git commit -a -m %1
 git pull origin pages/fr
 git pull origin pages/ja
 git push
