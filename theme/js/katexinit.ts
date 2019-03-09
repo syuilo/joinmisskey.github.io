6c6087602387992d111faaba80d64fb1d1bc9f42
tamaina <tamaina@hotmail.co.jp>
2019-03-10 02:30:37 +0900

    fix math

--- theme/js/katexinit.ts
+++ theme/js/katexinit.ts
@@ -1,12 +1,11 @@
-import { renderToString as getKatexString } from "katex"
+import { render } from "katex"
 
 export const katexinit = () => {
-    for ( const el of Array.from(document.querySelectorAll('[data-mfm^="math"]')) ) {
-        const n = document.createElement("span")
-        n.setAttribute("data-mfm", "math")
-        const s = getKatexString(el.textContent, { throwOnError: false })
-        n.insertAdjacentHTML("afterbegin", s)
-        el.insertAdjacentElement("beforebegin", n)
-        el.parentNode.removeChild(el)
+    for ( const el of Array.from(document.querySelectorAll('[data-mfm^="math"]')) as HTMLElement[] ) {
+        const f = el.textContent
+        el.textContent = ""
+        render(f, el, {
+            throwOnError: false
+        })
     }
 }
