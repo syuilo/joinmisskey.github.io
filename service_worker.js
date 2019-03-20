3f3f0b87891d52fbba801a4fd972087f087c8d72
tamaina <tamaina@hotmail.co.jp>
2019-03-21 03:24:33 +0900

    fix bug?

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2019-03-19T22:22:22.651Z */
+/* workbox 2019-03-20T18:15:54.937Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener("install", function(event) {
@@ -15,7 +15,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: "/offline/",
-        revision: "1553034142651",
+        revision: "1553105754937",
     }
 ]);
 self.addEventListener("fetch", function(event) {
