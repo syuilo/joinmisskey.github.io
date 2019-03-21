6044a14cf394489eb2f73075ffdb40fbb2b41517
tamaina <tamaina@hotmail.co.jp>
2019-03-22 07:55:07 +0900

    update script

--- theme/js/katexinit.ts
+++ theme/js/katexinit.ts
@@ -1,6 +1,14 @@
 export const katexinit = () => {
   const els = Array.from(document.querySelectorAll('[data-mfm^="math"]')) as HTMLElement[]
   if (els.length > 0) {
+    const href = "https://cdn.jsdelivr.net/npm/katex@0.10.0/dist/katex.min.css"
+    if (!document.querySelector(`link[href="${href}"]`)) {
+      const link = document.createElement("link")
+      link.setAttribute("href", href)
+      link.setAttribute("rel", "stylesheet")
+      document.head.insertAdjacentElement("beforeend", link)
+    }
+
     import("katex")
       .then(({ render }) => {
         for (const el of els ) {
