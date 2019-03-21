2b7576c21d117988ac9658c272b0b2aed4501349
tamaina <tamaina@hotmail.co.jp>
2019-03-21 21:15:45 +0900

    many padding

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2019-03-21T11:06:38.504Z */
+/* workbox 2019-03-21T12:10:59.415Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener("install", function(event) {
@@ -15,7 +15,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: "/offline/",
-        revision: "1553166398504",
+        revision: "1553170259415",
     }
 ]);
 self.addEventListener("fetch", function(event) {
