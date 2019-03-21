a5b5a24008ef99fcfe3e463be5d9926236b33936
tamaina <tamaina@hotmail.co.jp>
2019-03-22 08:58:41 +0900

    fix sw

--- theme/js/swinit.ts
+++ theme/js/swinit.ts
@@ -8,13 +8,13 @@ declare global {
 export const swinit = (): void => {
   if (typeof window.jm_pathToWorker === "string") {
     if ("serviceWorker" in navigator) {
-      const pastState = navigator.serviceWorker.controller.state
+      const pastState = navigator.serviceWorker.controller && navigator.serviceWorker.controller.state
       navigator.serviceWorker.register(window.jm_pathToWorker)
         .then(registration => {
           console.log("Service Worker: 登録: ", registration.scope)
           registration.addEventListener("updatefound", () => {
             console.log("updatefound", registration)
-            if (registration.installing && pastState !== "activated") {
+            if (registration.installing && pastState && pastState !== "activated") {
               registration.installing.onstatechange = () => {
                 console.log("Service Worker: バージョンアップします...")
                 location.reload(true)
