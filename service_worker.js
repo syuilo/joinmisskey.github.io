7b3ff2777f5ca04073aef9cbd86c97a1da423951
tamaina <tamaina@hotmail.co.jp>
2019-03-21 21:51:04 +0900

    less padding

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2019-03-21T12:40:11.549Z */
+/* workbox 2019-03-21T12:48:16.440Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener("install", function(event) {
@@ -15,7 +15,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: "/offline/",
-        revision: "1553172011549",
+        revision: "1553172496440",
     }
 ]);
 self.addEventListener("fetch", function(event) {
