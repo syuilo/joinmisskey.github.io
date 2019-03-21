cf5fca13329e4946bfffdd0538d9a23544473d6a
tamaina <tamaina@hotmail.co.jp>
2019-03-21 20:13:10 +0900

    KaTeXを外に

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2019-03-21T05:57:32.863Z */
+/* workbox 2019-03-21T11:06:38.504Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener("install", function(event) {
@@ -15,7 +15,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: "/offline/",
-        revision: "1553147852863",
+        revision: "1553166398504",
     }
 ]);
 self.addEventListener("fetch", function(event) {
