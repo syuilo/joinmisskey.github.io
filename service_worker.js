5cfe6b7645eb9edf161e06ea02a7ef63e75a4860
tamaina <tamaina@hotmail.co.jp>
2019-03-22 08:28:35 +0900

    update script

--- service_worker.js
+++ service_worker.js
@@ -1,23 +1,8 @@
-/* workbox 2019-03-21T22:50:08.266Z */
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
+/* workbox 2019-03-21T23:22:36.476Z */
 self.addEventListener("install", function(event) {
   workbox.skipWaiting();
   workbox.clientsClaim();
 })
-
-workbox.routing.registerRoute(
-    /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
-    workbox.strategies.staleWhileRevalidate({
-        cacheName: "assets-cache",
-    })
-);
-workbox.precaching.precacheAndRoute([
-    {
-        url: "/offline/",
-        revision: "1553208608266",
-    }
-]);
 self.addEventListener("fetch", function(event) {
     event.respondWith(
         caches.match(event.request)
@@ -29,3 +14,17 @@ self.addEventListener("fetch", function(event) {
         })
     );
 });
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.routing.registerRoute(
+    /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: "assets-cache",
+    })
+);
+workbox.precaching.precacheAndRoute([
+    {
+        url: "/offline/",
+        revision: "1553210556476",
+    }
+]);
