9f8fbc7fd5bd94f5a946e33f2c73ab3ed6024c4a
tamaina <tamaina@hotmail.co.jp>
2019-03-22 22:46:29 +0900

    revive pjax

--- theme/js/pjax-ready-others.ts
+++ theme/js/pjax-ready-others.ts
@@ -11,18 +11,18 @@ declare global {
 }
 
 export const pjaxLoaded = (): void => {
-  if(window.gtag){
-    window.gtag('event', 'page_view')
+  if (window.gtag) {
+    window.gtag("event", "page_view")
   }
-  if(window.DISQUS){
+  if (window.DISQUS) {
     // DISQUSでresetを送信します。
     // disqus_configはページ内に別に定義されています。
     window.DISQUS.reset({
-      reload: true,
-      config: window.disqus_config
+      config: window.disqus_config,
+      reload: true
     })
   }
-  if (window.location.search.indexOf('moved') >= 0) {
-    history.replaceState(null, null, window.location.href.replace(/\?.*$/,""))
+  if (window.location.search.indexOf("moved") >= 0) {
+    history.replaceState(null, null, window.location.href.replace(/\?.*$/, ""))
   }
 }
