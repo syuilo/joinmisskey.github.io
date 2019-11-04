3095601e431ffc118750ee626cd49365c07f9bfa
tamaina <tamaina@hotmail.co.jp>
2019-11-04 15:04:26 +0900

    :v:

--- .github/workflows/worktrees.sh
+++ .github/workflows/worktrees.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-git config --global user.name "luckybeast"
-git config --global user.email "remu.teshigawara@gmail.com"
-git fetch
-git checkout src
 git worktree add docs master
 git worktree add dist dist
 git worktree add pages/ja pages/ja
