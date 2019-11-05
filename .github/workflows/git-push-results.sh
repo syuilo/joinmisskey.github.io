da95351d44293c9bcfa3521b05c014f870408f22
tamaina <tamaina@hotmail.co.jp>
2019-11-05 12:59:03 +0900

    sha

--- .github/workflows/git-push-results.sh
+++ .github/workflows/git-push-results.sh
@@ -2,10 +2,10 @@
 git fetch
 cd docs
 git add --all
-git commit -a -m auto-built
+git commit -a -m "auto-built @$GITHUB_SHA"
 git push --force-with-lease origin master
 cd ../dist
 git add --all
-git commit -a -m auto-built
+git commit -a -m "auto-built @$GITHUB_SHA"
 git push --force-with-lease origin dist
 cd ..
