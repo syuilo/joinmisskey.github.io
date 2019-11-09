e6729646280e2cc3ecc6ac7cbe60389388449353
tamaina <tamaina@hotmail.co.jp>
2019-11-09 19:09:49 +0900

    fix w:build:assets

--- theme/js/gad.ts
+++ theme/js/gad.ts
@@ -7,12 +7,30 @@ declare global {
   }
 }
 
-export const gad = () => {
-  document.querySelectorAll("ins.adsbygoogle:not([data-adsbygoogle-status='done'])").forEach(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({})
-    } catch (e) {
-      console.error(e)
-    }
-  })
+const adpush = (target?: Element, o?: IntersectionObserver): void => {
+  try {
+    (window.adsbygoogle = window.adsbygoogle || []).push({})
+  } catch (e) {
+    console.error(e)
+  }
+  if (target && o) o.unobserve(target)
+}
+
+export const gad = (): void => {
+  const els = document.querySelectorAll("ins.adsbygoogle:not([data-adsbygoogle-status='done'])")
+  if (IntersectionObserver !== undefined && "IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((entries, o) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting) adpush(entry.target, o)
+        }
+      }, {
+          rootMargin: "60% 0px",
+          threshold: 0
+      })
+
+    els.forEach(el => observer.observe(el))
+  } else {
+    console.log("v")
+    els.forEach(() => adpush())
+  }
 }
--- theme/js/scrolltoshow.ts
+++ theme/js/scrolltoshow.ts
@@ -4,20 +4,20 @@ export const scrolltoshow = (): void => {
   const els = Array.from(document.getElementsByClassName("scrollts"))
   if (IntersectionObserver !== undefined && "IntersectionObserver" in window) {
     const observer = new IntersectionObserver((entries, o) => {
-      for (const entry of entries) {
-        if (entry.isIntersecting) tsShow(entry.target, o)
-      }
-    }, {
-        rootMargin: "-30% 0px",
-        threshold: 0
+        for (const entry of entries) {
+          if (entry.isIntersecting) tsShow(entry.target, o)
+        }
+      }, {
+          rootMargin: "-30% 0px",
+          threshold: 0
       })
     const observerNomargin = new IntersectionObserver((entries, o) => {
-      for (const entry of entries) {
-        if (entry.isIntersecting) tsShow(entry.target, o)
-      }
-    }, {
-        rootMargin: "0px",
-        threshold: 0
+        for (const entry of entries) {
+          if (entry.isIntersecting) tsShow(entry.target, o)
+        }
+      }, {
+          rootMargin: "0px",
+          threshold: 0
       })
     for (const el of els) {
       if (el.classList.contains("scrollts-nomargin")) observerNomargin.observe(el)
