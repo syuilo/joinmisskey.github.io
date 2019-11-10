a3b22445791539318a6630238657aaf2ebcb5355
tamaina <tamaina@hotmail.co.jp>
2019-11-10 20:18:49 +0900

    fix

--- scripts/better_markdown.js
+++ scripts/better_markdown.js
@@ -27,7 +27,7 @@ module.exports = (htm, urlprefix) => {
   $("h2, h3, h4, h5, h6").each((i) => {
     const $els = $("h2, h3, h4, h5, h6")
     $els.eq(i).attr("id", hs[i])
-    if (($els.length <= 7 && i === 1) || ($els.length > 7 && i === 4)) {
+    if (($els.length < 7 && i === 0) || ($els.length >= 7 && i === 4)) {
       // eslint-disable-next-line quotes
       $els.eq(i).before(`<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-1736621122676736" data-ad-slot="9247410221"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>`)
     }
