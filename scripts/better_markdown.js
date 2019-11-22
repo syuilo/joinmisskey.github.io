473e292712641d293433596f0d8101866dbf3fa5
tamaina <tamaina@hotmail.co.jp>
2019-11-22 10:47:45 +0900

    :v:

--- scripts/better_markdown.js
+++ scripts/better_markdown.js
@@ -32,7 +32,7 @@ module.exports = (htm, urlprefix) => {
     const $els = $("body > *")
     if ($els.length - (i + 1) >= 12 && i % 18 === 3) {
       // eslint-disable-next-line quotes
-      $els.eq(i).before(`<ins class="adsbygoogle mt-5" style="display:block; text-align:left;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-1736621122676736" data-ad-slot="9247410221"></ins>`)
+      $els.eq(i).before(`<ins class="adsbygoogle my-4" style="display:block; text-align:left;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-1736621122676736" data-ad-slot="9247410221"></ins>`)
     }
   })
   $("img").attr("loading", "lazy")
