f3ef1cf3615caa91d3585a7dc75f44e574168f0f
luckybeast <remu.teshigawara@gmail.com>
2019-12-13 07:39:18 +0000

    auto-built from cba2d16c882079f1034be69e743fd2709bc15bad

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2019-12-07T09:01:07.648Z */
+/* workbox 2019-12-13T07:38:26.823Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.skipWaiting();
@@ -51,7 +51,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: "/offline",
-        revision: "1575709267648",
+        revision: "1576222706823",
     }
 ]);
 
